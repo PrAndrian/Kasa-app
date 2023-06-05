5338cb3d35d455ba8765342c708935dbb6251eca
PrAndrian <princydruh@gmail.com>
2023-06-05 22:39:25 +0200

    navbar responsive

--- src/components/global/NavBar.js
+++ src/components/global/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import logo from '../../images/logo.png'
 import styles from '../../App.module.css'
 
@@ -7,12 +7,15 @@ const NavBar = () => {
 
   return (
     <nav className={styles.nav}>
-      <img src={logo} alt="Logo" className={styles.logo}/>
+      <Link to="/accueil">
+        <img src={logo} alt="Logo" className={styles.logo}/>
+      </Link>
+
       <div id={styles.links}>
-        <NavLink to="/accueil" className={({ isActive }) => isActive ? styles.active : ''}>
+        <NavLink to="/accueil" className={({ isActive }) => isActive ? styles.active : ''} end>
           Accueil
         </NavLink>
-        <NavLink to="/a-propos" className={({ isActive }) => isActive ? styles.active : ''}>
+        <NavLink to="/a-propos" className={({ isActive }) => isActive ? styles.active : ''} end>
           A propos
         </NavLink>
       </div>

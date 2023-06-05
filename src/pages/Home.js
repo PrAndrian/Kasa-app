82ae4ccf3f58b441d0b9104a6fb4e47d8a3611e3
PrAndrian <princydruh@gmail.com>
2023-06-05 23:40:18 +0200

    landscape card css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styles from '../App.module.css'
 
 const Home = () => {
   const logementId = 'testId'
   return (
-    <div>
-        This is the home page
-        <h2>Liste de logements</h2>
+    <div id={styles.home}>
+        <div className={styles.banner}>
+          <h2 className={styles.banner_title}>
+            <span className={styles.banner_text_1}>Chez vous,</span>
+            <span className={styles.banner_text_2}>partout et ailleurs</span></h2>
+        </div >
         <Link to={`/fiche-logement/${logementId}`}>Lien logement</Link>
     </div>
   )

4d33ef1f2fc4e088ad3eb6003da274367584ce4b
PrAndrian <princydruh@gmail.com>
2023-06-06 22:08:07 +0200

    banner responsive

---
+++ src/components/Thumb.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../App.module.css'
+
+const Thumb = () => {
+  return (
+    <div className={styles.thumb}>
+        
+    </div>
+  )
+}
+
+export default Thumb
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,18 @@ import styles from '../App.module.css'
 
 const Home = () => {
   const logementId = 'testId'
+  
   return (
     <div id={styles.home}>
         <div className={styles.banner}>
           <h2 className={styles.banner_title}>
             <span className={styles.banner_text_1}>Chez vous,</span>
-            <span className={styles.banner_text_2}>partout et ailleurs</span></h2>
+            <span className={styles.banner_text_2}> partout et ailleurs</span></h2>
         </div >
+
+        <section className={styles.thumbs_container}>
+          
+        </section>
         <Link to={`/fiche-logement/${logementId}`}>Lien logement</Link>
     </div>
   )

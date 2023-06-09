905812c7d860516cbed648410f1252a76df0026a
PrAndrian <princydruh@gmail.com>
2023-06-09 12:33:30 +0200

    Thumbs background image in JSX

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -2,9 +2,13 @@ import React from 'react'
 import styles from '../App.module.css'
 
 const Thumb = () => {
+  const image = "https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg"
   return (
-    <div className={styles.thumb}>
-        <p>Titre de la location</p>
+    <div 
+      className={styles.thumb} 
+      style={{ backgroundImage:`url(${image})`}}
+    >
+      <p>Titre de la location</p>
     </div>
   )
 }

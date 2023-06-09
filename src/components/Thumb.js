a08de136b68fca10906e0db1ac46ac105e65549f
PrAndrian <princydruh@gmail.com>
2023-06-09 12:35:02 +0200

    Thumbs background image shadow added

--- src/components/Thumb.js
+++ src/components/Thumb.js
@@ -6,7 +6,7 @@ const Thumb = () => {
   return (
     <div 
       className={styles.thumb} 
-      style={{ backgroundImage:`url(${image})`}}
+      style={{ backgroundImage:`linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%),url(${image})`}}
     >
       <p>Titre de la location</p>
     </div>

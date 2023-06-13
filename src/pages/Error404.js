0fb2d27eea32c61d977e497f7fdef73cd9f64a4a
PrAndrian <princydruh@gmail.com>
2023-06-13 23:03:09 +0200

    page 404 done

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from '../App.module.css'
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <div id={styles.page404}>
+      <h1 className={styles.error404}>404</h1>
+      <p className={styles.sentence404}>Oups! La page que vous demandez n'existe pas.</p>
+      <Link className={styles.link404} to="/">Retourner sur la page d’accueil</Link> 
+    </div>
   )
 }
 
